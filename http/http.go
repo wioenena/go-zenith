5956ad0549551b9001a875390875a8d33dea54ad
wioenena <wioenena@gmail.com>
2025-10-25 07:56:03 +0300

    add http package with HttpMethod constants and HttpContext interface

---
+++ http/http.go
@@ -0,0 +1,11 @@
+package http
+
+type HttpMethod string
+
+const (
+	GET    HttpMethod = "GET"
+	POST   HttpMethod = "POST"
+	PUT    HttpMethod = "PUT"
+	PATCH  HttpMethod = "PATCH"
+	DELETE HttpMethod = "DELETE"
+)
